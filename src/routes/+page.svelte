c548fa1e996a89c9332cf0a884fefec099b13242
Feroz Mujawar <ferozm@heaptrace.com>
2024-05-06 20:32:16 +0530

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+</script>
+
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
+
+<style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { join } from 'path';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}',join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms'), skeleton]
+}
+
